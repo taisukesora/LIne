206796260db5bcecd711c130f452a1dcdb1fc187
Yusuke Ujitoko <nagasuyusuke@gmail.com>
2014-08-03 12:26:17 +0900

    linefirst

---
+++ Waterphysics/Assets/Scripts/LineTest.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LineTest : MonoBehaviour {
+  public Transform[] transform;
+  LineRenderer lineRenderer;
+  Vector3 start, direction;
+  float length, velocity, range, time;
+  // Use this for initialization
+  void Start () {
+    lineRenderer = GetComponent<LineRenderer>();
+    lineRenderer.SetVertexCount(2);
+    range = 10.0f;
+    start = new Vector3(Random.Range(-range,range), 
+			Random.Range(-range,range),
+			Random.Range(-range,range));
+    direction = new Vector3(Random.Range(-range,range), 
+			    Random.Range(-range,range), 
+			    Random.Range(-range,range));
+    direction.Normalize();
+    velocity = 1.0f;
+
+    lineRenderer.SetWidth(0.1f, 0.01f);
+  }
+  
+  // Update is called once per frame
+  void Update () {
+    time += Time.deltaTime;
+    
+    Vector3 s = start+direction*time*velocity;
+    
+    //範囲外の時
+    if(s.sqrMagnitude>300.0f)
+      {	
+        time = 0.0f;
+	//
+	direction = -direction;
+	start = s;
+//	s = start+direction*time*velocity;
+      }
+    Vector3 e = s + direction*1.0f;
+    
+    
+    lineRenderer.SetPosition(0, s);
+    lineRenderer.SetPosition(1, e);
+    
+    
+  }
+
+  
+  Vector3 reset()
+  {
+    start = new Vector3(Random.Range(-range,range), 
+			Random.Range(-range,range),
+			Random.Range(-range,range));
+    direction = new Vector3(Random.Range(-range,range), 
+			    Random.Range(-range,range), 
+			    Random.Range(-range,range));
+    return (start+direction*time*velocity);
+  }
+  
+}
---
+++ Waterphysics/Assets/linegeneration.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class linegeneration : MonoBehaviour {
+  public GameObject lineprefab;
+  public float num_line;
+  // Use this for initialization
+  void Start () {
+    for(int i=0;i<num_line;i++)
+      {
+	Instantiate(lineprefab, Vector3.zero, Quaternion.identity);
+      }
+  }
+  
+  // Update is called once per frame
+  void Update () {
+    
+  }
+}
